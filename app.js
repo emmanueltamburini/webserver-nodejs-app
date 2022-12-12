6d1afad580bfb94e4143f9096c6e4485c8aa03ce
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-12-11 23:48:05 -0400

    app with hbs has done

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 import express from 'express';
 import * as url from 'url';
+import hbs from 'hbs';
+
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const app = express();
 const port = 8080;
+const options = {
+    name: 'Emmanuel',
+    title: 'Node with express app'
+};
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
 
 // Serve static content
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.render('home', options);
+});
+
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic', options);
 });
 
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
+    res.render('elements', options);
 });
 
 app.get('*',(req, res) => {
