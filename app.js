042eddfd62597346963d2ff96034be6e69aa4b2f
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-12-11 22:56:34 -0400

    first example how to serve static content

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 import express from 'express';
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Home page');
-});
+// Serve static content
+app.use(express.static('public'))
 
-app.get('/hello-world', (req, res) => {
-    res.send('Hello World page');
+app.get('/basic-response', (req, res) => {
+    res.send('Basic Response page');
 });
 
 app.get('*',(req, res) => {
-  res.send('404 | Page not found');
+  console.log(__dirname);
+  res.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
